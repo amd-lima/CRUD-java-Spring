d58b01a184f22e34d6618db0252e7fa15e62c63c
AmandaLimaLuiz <delimaluiz.amanda@gmail.com>
2023-03-22 10:38:56 -0300

    push

--- src/main/java/com/amanda/controller/CourseController.java
+++ src/main/java/com/amanda/controller/CourseController.java
@@ -2,11 +2,13 @@ package com.amanda.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.amanda.model.Course;
@@ -27,7 +29,8 @@ public class CourseController {
     }
 
     @PostMapping
-    public void create(@RequestBody Course course){
-
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course){
+       return courseRepository.save(course);
     }
 }
