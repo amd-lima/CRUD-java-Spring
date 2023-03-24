c1c3f86c36f73a0c5eb5f0c3e375f098802e4861
AmandaLimaLuiz <delimaluiz.amanda@gmail.com>
2023-03-24 15:28:41 -0300

    validation-pattern

--- src/main/java/com/amanda/model/Course.java
+++ src/main/java/com/amanda/model/Course.java
@@ -1,6 +1,8 @@
 package com.amanda.model;
 
 
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -12,12 +14,15 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 
 @Data
 @Entity
 //@Table(name = "cursos") -> caso queira criar a tabela em um banco já existente.
+@SQLDelete(sql = "UPDATE Course SET status = 'Inativo' WHERE id = ?" )
+@Where(clause = "status = 'Ativo'")
 public class Course {
 
     @Id
@@ -35,6 +40,13 @@ public class Course {
     @NotNull
     @Length(max = 12)
     @Column(length = 12, nullable = false)
+    @Pattern(regexp = "Front-end|Back-end")
     private String category;
+
+    @NotNull
+    @Length(max = 12)
+    @Column(length = 12, nullable = false)
+    @Pattern(regexp = "Ativo|Inativo")
+    private String status = "Ativo";
 
 }
