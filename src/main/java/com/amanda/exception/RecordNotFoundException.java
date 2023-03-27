d03804f371476cb56be2141bdb8c88c76356510d
AmandaLimaLuiz <delimaluiz.amanda@gmail.com>
2023-03-27 09:25:15 -0300

    add exception

--- src/main/java/com/amanda/exception/RecordNotFoundException.java
+++ src/main/java/com/amanda/exception/RecordNotFoundException.java
@@ -2,8 +2,6 @@ package com.amanda.exception;
 
 public class RecordNotFoundException extends RuntimeException{
 
-    private static final Long serialVersionUID = 1L;
-
     public RecordNotFoundException(Long id){
         super("Registro não encontrado com o id: " + id);
     }
--- src/main/java/com/amanda/repository/CourseRepository.java
+++ src/main/java/com/amanda/repository/CourseRepository.java
@@ -1,11 +1,11 @@
 package com.amanda.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import com.amanda.model.Course;
 
-@Repository
+
 public interface CourseRepository extends JpaRepository<Course, Long>{
     
 }
